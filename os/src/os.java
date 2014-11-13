8f7c0f055970e2adb600ba9abb9e675955458473
CISC3320Project <el-3mem2@hotmail.com>
2014-11-13 03:09:32 -0500

    initial commit

---
+++ os/src/os.java
@@ -0,0 +1,27 @@
+public class os {
+
+	
+    public static void main(String[] args) {
+        
+    	
+    }
+	public static void startup(){
+	
+	}
+	public static void Crint(int i[],int[]p){
+	//
+	}
+	public static void Svc(int i[],int[]p){
+	
+	}
+	public static void Tro(int i[],int[]p){
+	
+	}
+	public static void Dskint(int i[],int[]p){
+	
+	}
+	public static void Drmint(int i[],int[]p){
+	
+	}
+    
+}
