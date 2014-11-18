27c2b0958eaa88c1b96bdd3cd370012dfb54a39e
Alex Victorino <el-3mem2@hotmail.com>
2014-11-18 06:07:27 -0500

    Update os.java
    added more comments

--- os/src/os.java
+++ os/src/os.java
@@ -1,27 +1,45 @@
-public class os {
+import java.util.ArrayList;
 
-	
-    public static void main(String[] args) {
-        
-    	
-    }
+public class os {
+	static ArrayList<jobtype> jobs = new ArrayList<jobtype>();
+    	static int currentTime=0;
+
 	public static void startup(){
-	
+		sos.offtrace();
 	}
-	public static void Crint(int i[],int[]p){
-	//
+	public static void Crint(int []a,int[]p){
+	p[5] = currentTime;
+		jobtype job = new jobtype();
+		p[1] = job.JobNo();
+		p[2] = job.Priority();
+		p[3] = job.Size();
+		p[4] = job.MaxCpuTime();
+		p[5] = job.TermCpuTime();
+		jobs.add(job);
 	}
-	public static void Svc(int i[],int[]p){
-	
+	public static void Svc(int []a,int[]p){
+	  p[5] = currentTime;
+		if(a[0]==5){
+			
+		}
+		if(a[0]==6){
+		
+		}
+		if(a[0]==7){
+		
+		}
 	}
-	public static void Tro(int i[],int[]p){
-	
+	public static void Tro(int []a,int[]p){
+	//Timer-Run-Out
+		p[5] = currentTime;
 	}
-	public static void Dskint(int i[],int[]p){
-	
+	public static void Dskint(int []a,int[]p){
+	//Disk interrupt
+		p[5] = currentTime;
 	}
-	public static void Drmint(int i[],int[]p){
-	
+	public static void Drmint(int []a,int[]p){
+	//Drum interrupt
+		p[5] = currentTime;
 	}
     
 }
